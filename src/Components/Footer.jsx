5296ddea076c43fe4db8b4439403d6713760599b
Johnson Prasad <jtprasad@ymail.com>
2026-01-17 18:29:26 -0700

    Create Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Footer = () => {
+    return (
+        <>
+        <footer>
+            <p>&copy; Event Planner Organization. All rights reserved.</p>
+            <p>Brought to you by Tony Prasad</p>
+        </footer>
+        </>
+    )}
+
+export default Footer
